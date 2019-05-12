73d27c2064296a04d1e887e2f195d35545450e51
saroj <saroj261@gmail.com>
2019-05-12 19:51:03 +0530

    first commit

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,27 @@
+package com.in28minutes.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+ int maximum;
+ int minimum;
+ protected LimitConfiguration() {
+	 
+ }
+ public int getMaximum() {
+	return maximum;
+}
+public void setMaximum(int maximum) {
+	this.maximum = maximum;
+}
+
+public int getMinimum() {
+	return minimum;
+}
+public void setMinimum(int minimum) {
+	this.minimum = minimum;
+}
+public LimitConfiguration(int maximum, int minimum) {
+	super();
+	this.maximum = maximum;
+	this.minimum = minimum;
+}
+}
